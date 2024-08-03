9a31c1dbb4190bebd734d5bc5b45124f65aac437
Yash Dodiya <yashdodiya13022004@gmail.com>
2024-08-03 19:13:13 +0530

    add 9 create popup project

---
+++ 9_Create_popups/script.js
@@ -0,0 +1,24 @@
+// One way
+/*
+const openPopup = document.querySelector('.open-popup')
+const closeIcon = document.querySelector('.close-icon')
+const popup = document.querySelector('.popup')
+const overlay = document.querySelector('.overlay')
+const overlayOpen = document.querySelector('.overlay-open')
+
+console.log(overlay);
+console.log(overlayOpen);
+
+
+openPopup.addEventListener("click", (e) => {
+    popup.classList.add('open')
+    overlay.classList.add('overlay-open')
+})
+
+closeIcon.addEventListener("click", (e) => {
+    popup.classList.remove('open')
+    overlay.classList.remove('overlay-open')
+})
+*/
+
+// Second way
